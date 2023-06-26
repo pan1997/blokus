c93fbaa81171bce32ecac5ff962341613ebaa15e
Pankaj Kumar <pankajkumaryadav1997@gmail.com>
2023-06-26 16:33:13 -0700

    pytorch onnx tutorial

---
+++ ai/nn/py/test_torch.py
@@ -0,0 +1,42 @@
+import torch
+import warnings
+
+device = "cuda" if torch.cuda.is_available() else "cpu" if torch.backends.mps.is_available() else "cpu"
+print(device)
+
+
+class Net(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.flatten = torch.nn.Flatten()
+        self.ll = torch.nn.Sequential(
+            torch.nn.Linear(6, 5),
+            torch.nn.ReLU(),
+            torch.nn.Linear(5, 2),
+        )
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.ll(x)
+        return logits
+
+
+model = Net().to(device)
+print(model)
+x = torch.Tensor([[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]]).to(device)
+y = model(x)
+print(y)
+
+torch.onnx.export(
+    model,
+    x,
+    "test.onnx",
+    export_params=True,
+    opset_version=10,
+    do_constant_folding=True,
+    input_names=["input"],
+    output_names=["output"],
+    dynamic_axes={
+        "input": {0: "batch_size"},
+        "output": {0: "batch_size"}
+    }
+)
--- ai/nn/src/main.rs
+++ ai/nn/src/main.rs
@@ -1,3 +1,35 @@
+use ndarray::{Array1};
+use ort::{Environment, ExecutionProvider, SessionBuilder, tensor::{InputTensor, FromArray, OrtOwnedTensor}};
+use std::sync::Arc;
+
+mod lib;
+
 fn main() {
+  tracing_subscriber::fmt::init();
+  let environment = 
+    Arc::new(
+      Environment::builder()
+        .with_execution_providers([ExecutionProvider::coreml()])
+        .build()
+        .unwrap()
+    );
+  
+  let session = 
+    SessionBuilder::new(&environment)
+      .unwrap()
+      .with_optimization_level(ort::GraphOptimizationLevel::Level1)
+      .unwrap()
+      .with_intra_threads(1)
+      .unwrap()
+      .with_model_from_file("/Users/pankaj/Learning/blokus/ai/nn/py/test.onnx")
+      .unwrap();
+
+  let array = Array1::from_iter((1..7).map(|i| i as f32)).into_shape((1, 2, 3)).unwrap();
+
+  let res = session.run([InputTensor::from_array(array.into_dyn())]).unwrap();
+  let output: OrtOwnedTensor<f32, _> = res[0].try_extract().unwrap();
+  let rr = output.view();
+  let r = rr.as_slice().unwrap();
+  println!("{:?}", r);
   println!("Hello, world!");
 }
